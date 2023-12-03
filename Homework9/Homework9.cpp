377de0c69da1d59aa20f25f3127b0ed1654e1c7e
unknown <Dmytro>
2023-12-03 15:28:00 +0200

    Homework 9

---
+++ Homework9/Homework9.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+#include "Utils.h"
+
+const int numbersArraySize = 10;
+const char charsArraySize = 5;
+const int stringMaxSize = 20;
+
+int main()
+{
+    std::cout << "Homework 9!\n";
+
+    breakLine("Task #1");
+
+    std::cout << "Please, input 10 numbers: " << std::endl;
+
+    int numbers[numbersArraySize]{};
+    int i = 0;
+
+    do {
+        std::cin >> numbers[i];
+        i++;
+    } while (i < numbersArraySize);
+
+    std::cout << "Processing ... " << std::endl;
+
+    translateArray(numbers, numbersArraySize);
+
+    breakLine("Task #2");
+
+    char stringForUppercase[stringMaxSize]{};
+
+    // Should clear input 
+    std::cin.getline(stringForUppercase, stringMaxSize);
+
+    std::cout << "Enter string for uppercase: ";
+    std::cin.getline(stringForUppercase, stringMaxSize);
+
+    toUppercase(stringForUppercase);
+
+    breakLine("Task #3");
+
+    std::string word;
+
+    std::cout << "Enter word for palindrom test: ";
+    std::getline(std::cin, word);
+    
+    std::cout << "Is correct: " << std::boolalpha << isPalindrom(word) << std::endl;
+
+    breakLine("Task #4");
+
+    char str[stringMaxSize]{};
+
+    std::cout << "Enter word for count vowels and consonants: ";
+    std::cin.getline(str, stringMaxSize);
+
+    int vowelsCount = 0;
+    int consonantsCount = 0;
+
+    parseStringLetters(str, vowelsCount, consonantsCount);
+
+    std::cout << "Vowels count: " << vowelsCount << std::endl;
+    std::cout << "Consonants count: " << consonantsCount << std::endl;
+
+    breakLine("Task #5");
+
+    char str1[stringMaxSize]{};
+    char str2[stringMaxSize]{};
+
+    std::cout << "Enter first work for compare: ";
+    std::cin.getline(str1, stringMaxSize);
+
+    std::cout << "Enter second work for compare: ";
+    std::cin.getline(str2, stringMaxSize);
+
+    std::cout << "Is this string equal: " << std::boolalpha << isEqual(str1, str2) << std::endl;
+}
---
+++ Homework9/Utils.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+void breakLine(std::string title, const int symbolLength = 16)
+{
+    std::cout << std::endl << std::endl;
+    for (int i = 0; i < symbolLength; i++) {
+        std::cout << "*";
+    }
+
+    std::cout << std::endl << "*";
+
+    std::cout << " " << title << " ";
+
+    size_t clearSpace = symbolLength - title.length() - 4;
+
+    for (int i = 0; i < clearSpace; i++) {
+        std::cout << " ";
+    }
+
+    std::cout << "*" << std::endl;
+
+    for (int i = 0; i < symbolLength; i++) {
+        std::cout << "*";
+    }
+    std::cout << std::endl << std::endl;
+}
+
+void translateArray(int numbers[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            numbers[i] *= 2;
+        }
+        else if (numbers[i] < 0)
+        {
+            numbers[i] = 0;
+        }
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << numbers[i] << " ";
+    }
+}
+
+void toUppercase(char str[])
+{
+    for (int i = 0; i < sizeof(str); i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            std::cout << static_cast<char>(str[i] - 32);
+        }
+        else
+        {
+            std::cout << str[i];
+        }
+    }
+}
+
+bool isPalindrom(std::string word)
+{
+    std::string reversed;
+
+    int i = 0;
+
+    do {
+        reversed += word[i];
+        i++;
+    } while (word[i] != '\0');
+
+    i = 0;
+
+    do {
+        if (word[i] != reversed[i]) {
+            return false;
+        }
+        i++;
+    } while (word[i] != '\0');
+
+    return true;
+}
+
+void parseStringLetters(const char str[], int& vowelsCount, int& consonantsCount)
+{
+    for (int i = 0; str[i] != '\0'; i++) {
+        char currentChar = std::tolower(str[i]);
+
+        if (std::isalpha(currentChar)) {
+            switch (currentChar) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    vowelsCount++;
+                    break;
+                default:
+                    consonantsCount++;
+            }
+        }
+    }
+}
+
+bool isEqual(const char str1[], const char str2[]) 
+{
+    for (int i = 0; str1[i] != '\0'; i++) {
+        if (str1[i] != str2[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
